d2e54f128071d770b8d389031d9ef9a8c29d4cb1
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-04-13 21:41:32 +0300

    bad code

--- MT/allocators/SmallAllocator.cpp
+++ MT/allocators/SmallAllocator.cpp
@@ -5,10 +5,16 @@ class SmallAllocator {
 private:
         char Memory[1048576];
 	char *cp; //current offset pointer
+
 public:
         void *Alloc(unsigned int Size) {
-		size_t *sz = (size_t*)(cp + sizeof(size_t));
+		cp += sizeof(size_t);
+		size_t *sz = (size_t *) cp;
 		*sz = Size;
+		std::cout << "Allocating size: " << *sz << std::endl;		
+		char *result = ++cp;
+		cp += Size - 1;
+		return result;
 	};
         void *ReAlloc(void *Pointer, unsigned int Size) {};
         void Free(void *Pointer) {};
@@ -16,5 +22,8 @@ public:
 
 
 int main(int argc, char **argv) {
-	std::cout << "Small Allocator" << std::endl;
+	SmallAllocator sa;
+	int *i = (int *) sa.Alloc(sizeof(int));
+	std::cout << "After Alloc";
+	std::cout << "Value at allocated area: " << *i << std::endl;
 }
