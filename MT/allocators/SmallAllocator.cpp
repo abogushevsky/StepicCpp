974dc7ee8d059aaec9350f000693d33f1bbfafb3
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-04-13 22:40:47 +0300

    Alloc done

--- MT/allocators/SmallAllocator.cpp
+++ MT/allocators/SmallAllocator.cpp
@@ -1,20 +1,26 @@
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
+
+#define SZ 1048576
 
 class SmallAllocator {
 private:
-        char Memory[1048576];
+        char Memory[SZ];
+	int offset = 0;
 	char *cp; //current offset pointer
-
 public:
         void *Alloc(unsigned int Size) {
-		cp += sizeof(size_t);
-		size_t *sz = (size_t *) cp;
+		offset += sizeof(size_t);		
+		if (offset >= SZ) return NULL;
+		size_t *sz = (size_t *) Memory + offset;
 		*sz = Size;
 		std::cout << "Allocating size: " << *sz << std::endl;		
-		char *result = ++cp;
-		cp += Size - 1;
-		return result;
+		
+		if (offset >= SZ) return NULL;
+		void *res = Memory + offset;
+		offset += Size;
+		return res;
 	};
         void *ReAlloc(void *Pointer, unsigned int Size) {};
         void Free(void *Pointer) {};
@@ -24,6 +30,7 @@ public:
 int main(int argc, char **argv) {
 	SmallAllocator sa;
 	int *i = (int *) sa.Alloc(sizeof(int));
-	std::cout << "After Alloc";
+	std::cout << "After Alloc" << std::endl;
+	*i = 16;
 	std::cout << "Value at allocated area: " << *i << std::endl;
 }
