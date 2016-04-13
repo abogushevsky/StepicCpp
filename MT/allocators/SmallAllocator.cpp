5c899dedcb5758de32ca0849c5f089792b75a9ce
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-04-13 21:26:33 +0300

    first steps & ignoring exes

--- MT/allocators/SmallAllocator.cpp
+++ MT/allocators/SmallAllocator.cpp
@@ -1,8 +1,20 @@
+#include <stdlib.h>
+#include <iostream>
+
 class SmallAllocator {
 private:
         char Memory[1048576];
+	char *cp; //current offset pointer
 public:
-        void *Alloc(unsigned int Size) {};
+        void *Alloc(unsigned int Size) {
+		size_t *sz = (size_t*)(cp + sizeof(size_t));
+		*sz = Size;
+	};
         void *ReAlloc(void *Pointer, unsigned int Size) {};
         void Free(void *Pointer) {};
 };
+
+
+int main(int argc, char **argv) {
+	std::cout << "Small Allocator" << std::endl;
+}
