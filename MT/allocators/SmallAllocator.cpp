354e9a8b96e1ed5b25d065bbd0e868bbb2b8615c
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-04-13 20:20:01 +0300

    correct class template

--- MT/allocators/SmallAllocator.cpp
+++ MT/allocators/SmallAllocator.cpp
@@ -1,2 +1,8 @@
 class SmallAllocator {
-}
+private:
+        char Memory[1048576];
+public:
+        void *Alloc(unsigned int Size) {};
+        void *ReAlloc(void *Pointer, unsigned int Size) {};
+        void Free(void *Pointer) {};
+};
