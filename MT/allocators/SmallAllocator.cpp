16a06384ca4ba7b228b3453054e356c2c9a10eef
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-04-21 21:13:49 +0300

    ReAlloc fixed

--- MT/allocators/SmallAllocator.cpp
+++ MT/allocators/SmallAllocator.cpp
@@ -5,53 +5,55 @@
 
 class SmallAllocator {
 private:
-        char Memory[SZ];
+	char Memory[SZ];
 	int offset = 0;
 	char *cp; //current offset pointer
 
 	int getSize(void *pointer) const {
 		int sz = *((int *) pointer - 1); //get the allocated size value
-	        std::cout << "Object size is: " << sz << std::endl;
+		std::cout << "Object size is: " << sz << std::endl;
 		return sz;
 	};
-	
-	char* getAllocObjStart(void *pointer, int sz) {
+
+	char *getAllocObjStart(void *pointer, int sz) {
 		return ((char *) pointer) - (sz + sizeof(int) + 1);
 	}
+
 public:
-        void *Alloc(unsigned int Size) {
+	void *Alloc(unsigned int Size) {
 		if (offset + sizeof(int) + 1 + Size >= SZ) return NULL;
 		*(Memory + offset) = '1'; //set mark of occupied block
 		int *sz = (int *) (Memory + (offset + 1)); //1 is occupied mark block
 		*sz = Size;
-		std::cout << "Allocating size: " << *sz << std::endl;		
-		
+		std::cout << "Allocating size: " << *sz << std::endl;
+
 		offset += sizeof(int) + 1; //header offset added
 		void *res = Memory + offset;
 		offset += Size;
 		return res;
 	};
 
-        void *ReAlloc(void *Pointer, unsigned int Size) {
+	void *ReAlloc(void *Pointer, unsigned int Size) {
 		int sz = getSize(Pointer);
 		if (offset + sizeof(int) + 1 + sz >= SZ) return NULL;
 		char *start = getAllocObjStart(Pointer, sz);
 		//TODO: Move header, size & data to the current offset position		
 		//TODO: Free current allocated area
 		char *end = start + sz;
+		char *target = Memory + offset;
 		while (start != end) {
-			char *target = Memory + offset;
 			*target = *start;
+			target++;
 			start++;
 			offset++;
 		}
-		return NULL; //temp
+		return (void *)(target - sz - sizeof(int) - 1); //temp
 	};
 
-        void Free(void *Pointer) {
+	void Free(void *Pointer) {
 		int sz = getSize(Pointer);
 		char *ptr = getAllocObjStart(Pointer, sz); //sum of sz and header (size value + occupied mark)
-		while(ptr != ((char *)Pointer + sz)) {
+		while (ptr != ((char *) Pointer + sz)) {
 			*ptr = '\0';
 			ptr++;
 		}
