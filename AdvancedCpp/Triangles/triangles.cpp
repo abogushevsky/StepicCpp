da881da75182408e377bcaeab9dea5c3c21048b7
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-12-04 08:21:51 -0800

    overflow fixed

--- AdvancedCpp/Triangles/triangles.cpp
+++ AdvancedCpp/Triangles/triangles.cpp
@@ -1,31 +1,20 @@
 #include <iostream>
 
-long pow(long number, long pow) {
-    long result = 1;
+#define myType long long
 
-    for (long i = 1; i <= pow; i++) {
+ myType pow(myType number, myType pow) {
+     myType result = 1;
+
+    for (myType i = 1; i <= pow; i++) {
         result *= number;
     }
 
     return result;
 }
 
-/*int getTrianglePositionNumber1(int currentPos, long currentSum, long targetNumber) {
-    long nextSum = currentSum + pow(currentPos, 3);
-    if (nextSum < targetNumber) {
-        return getTrianglePositionNumber(++currentPos, nextSum, targetNumber);
-    }
-
-    if (nextSum == targetNumber) {
-        return currentPos;
-    }
-
-    return 0;
-}*/
-
-long getTrianglePositionNumber(long targetNumber) {
-    long currentSum = 0;
-    long currentPos = 1;
+myType getTrianglePositionNumber(myType targetNumber) {
+    myType currentSum = 0;
+    myType currentPos = 1;
 
     while (currentSum < targetNumber) {
         currentSum += pow(currentPos, 3);
@@ -35,8 +24,8 @@ long getTrianglePositionNumber(long targetNumber) {
     return currentSum == targetNumber ? currentPos - 1 : 0;
 }
 
-/*int getTrianglePositionNumber(long targetNumber) {
-    long currentNumber = 1;
+/*int getTrianglePositionNumber(myType targetNumber) {
+    myType currentNumber = 1;
     int result = 1;
 
     while (currentNumber < targetNumber) {
@@ -50,9 +39,13 @@ long getTrianglePositionNumber(long targetNumber) {
 int main(int argc, char **argv) {
     std::cout << "Hello triangles!" << std::endl;
 
-	for (long i = 0; i < 9223372036854775807; i++) {
+    /*myType num = getTrianglePositionNumber(pow(46360, 2));
+    std::cout << "Num is: " << 46360 << ", Pos: " << num << std::endl;
+    return 0;*/
+
+	for (myType i = 0; i < 9223372036854775807; i++) {
 		//int num = getTrianglePositionNumber(1, 0, pow(i, 2));
-        long num = getTrianglePositionNumber(pow(i, 2));
+        myType num = getTrianglePositionNumber(pow(i, 2));
         //int num = getTrianglePositionNumber(i);
 		if (num != 0) {
 			std::cout << "Num is: " << i << ", Pos: " << num << std::endl;
