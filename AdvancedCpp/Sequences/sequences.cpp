ae65457d2fb5a82ed885d7c53cf51357c4ade83f
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2016-12-18 20:30:07 +0300

    shit

--- AdvancedCpp/Sequences/sequences.cpp
+++ AdvancedCpp/Sequences/sequences.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string>
+
+std::string scanInput() {
+	std::string result = "";
+	std::string tmp;
+    
+    /*while(std::cin >> tmp) {
+        result += tmp + " ";
+		std::cout << "Current: " << result << std::endl;
+    }*/
+
+    std::cin >> result;
+    std::cout << "There are " << std::cin.rdbuf()->in_avail() << " symbols in input buffer" << std::endl;
+
+    return result;
+}
 
 int main(int argc, char **argv) {
-    
+	auto inputString  = scanInput();
+    std::cout << inputString << std::endl;
+    //std::floor(N) == N
+    return 0;
 }
