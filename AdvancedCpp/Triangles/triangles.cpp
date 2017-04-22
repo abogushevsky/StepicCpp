dea6d4783e68c6d2aea030cf800f212520beb523
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2017-04-22 20:02:38 +0400

    solving

--- AdvancedCpp/Triangles/triangles.cpp
+++ AdvancedCpp/Triangles/triangles.cpp
@@ -1,26 +1,38 @@
 #include <cstdio>
 #include <cmath>
 
-#define myType unsigned long long
+#define ull_int unsigned long long
 
-myType solveQuadraticEquation(myType triangleNumber) {
-	myType c = triangleNumber * 2;
-	myType d = 1 + (4 * c);
-	myType x1 = (-1 + sqrt(d)) / 2;
-	myType x2 = (-1 - sqrt(d)) / 2;
+ull_int solveQuadraticEquation(ull_int triangleNumber) {
+	ull_int c = triangleNumber * 2;
+	ull_int d = 1 + (4 * c);
+
+	if (d < 0) {
+		return 0;
+	}
+
+	ull_int x1 = (-1 + sqrt(d)) / 2;
+	ull_int x2 = (-1 - sqrt(d)) / 2;
 	return x1;
 }
 
-myType getSequentionalNumberOfTriangleNumber(myType triangleNumber) {	
-	return solveQuadraticEquation(triangleNumber);
+bool isCorrectTriangleNumber(ull_int sourceTriangleNumber, ull_int sequentialNumber) {
+	return sourceTriangleNumber == ((pow(sequentialNumber, 2) + sequentialNumber) / 2);
+}
+
+ull_int getSequentionalNumberOfTriangleNumber(ull_int triangleNumber) {	
+	ull_int result = solveQuadraticEquation(triangleNumber);
+	return isCorrectTriangleNumber(triangleNumber, result) ? result : 0;
 }
 
 int main(int argc, char **argv) {	
-	myType triangleNumber;
+	ull_int triangleNumber = 0;
 
-	printf("Enter a triangle number: ");
-	scanf("%llu", &triangleNumber);
-	printf("Trinangle number is: %llu", getSequentionalNumberOfTriangleNumber(triangleNumber));
-	scanf("%llu", &triangleNumber);
+	while (triangleNumber != -1) {
+		printf("Enter a triangle number: ");
+		scanf("%llu", &triangleNumber);
+		printf("Trinangle number is: %llu \r\n\r\n", getSequentionalNumberOfTriangleNumber(triangleNumber));
+	}
+
 	return 0;
 }
