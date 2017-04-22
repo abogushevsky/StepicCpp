04462bbeff2249f6df71f1a0f598aa97fac56052
Anton Bogushevsky <anton.bogushevsky@gmail.com>
2017-04-22 16:28:54 +0400

    Solving sqaure equation draft

--- AdvancedCpp/Triangles/triangles.cpp
+++ AdvancedCpp/Triangles/triangles.cpp
@@ -1,54 +1,26 @@
-#include <iostream>
+#include <cstdio>
+#include <cmath>
 
-#define myType long long
+#define myType unsigned long long
 
- myType pow(myType number, myType pow) {
-     myType result = 1;
-
-    for (myType i = 1; i <= pow; i++) {
-        result *= number;
-    }
-
-    return result;
+myType solveQuadraticEquation(myType triangleNumber) {
+	myType c = triangleNumber * 2;
+	myType d = 1 + (4 * c);
+	myType x1 = (-1 + sqrt(d)) / 2;
+	myType x2 = (-1 - sqrt(d)) / 2;
+	return x1;
 }
 
-myType getTrianglePositionNumber(myType targetNumber) {
-    myType currentSum = 0;
-    myType currentPos = 1;
-
-    while (currentSum < targetNumber) {
-        currentSum += pow(currentPos, 3);
-        currentPos++;
-    }
-
-    return currentSum == targetNumber ? currentPos - 1 : 0;
+myType getSequentionalNumberOfTriangleNumber(myType triangleNumber) {	
+	return solveQuadraticEquation(triangleNumber);
 }
 
-/*int getTrianglePositionNumber(myType targetNumber) {
-    myType currentNumber = 1;
-    int result = 1;
+int main(int argc, char **argv) {	
+	myType triangleNumber;
 
-    while (currentNumber < targetNumber) {
-        currentNumber = 0.5 * (result * (result + 1));
-        result++;
-    }
-
-    return currentNumber == targetNumber ? result - 1 : 0;
-}*/
-
-int main(int argc, char **argv) {
-    std::cout << "Hello triangles!" << std::endl;
-
-    /*myType num = getTrianglePositionNumber(pow(46360, 2));
-    std::cout << "Num is: " << 46360 << ", Pos: " << num << std::endl;
-    return 0;*/
-
-	for (myType i = 0; i < 9223372036854775807; i++) {
-		//int num = getTrianglePositionNumber(1, 0, pow(i, 2));
-        myType num = getTrianglePositionNumber(pow(i, 2));
-        //int num = getTrianglePositionNumber(i);
-		if (num != 0) {
-			std::cout << "Num is: " << i << ", Pos: " << num << std::endl;
-		}
-	}
+	printf("Enter a triangle number: ");
+	scanf("%llu", &triangleNumber);
+	printf("Trinangle number is: %llu", getSequentionalNumberOfTriangleNumber(triangleNumber));
+	scanf("%llu", &triangleNumber);
+	return 0;
 }
